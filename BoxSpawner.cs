ee470799e81222a3622b661db8f5cc35d5816054
lishiming <1402706018@qq.com>
2020-04-13 22:50:21 +0800

    第一次提交

---
+++ BoxSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject box_prefabs;
+
+    public void SpawnBox()
+    {
+        GameObject obj_box = Instantiate(box_prefabs);
+        Vector3 temp = transform.position;
+        temp.z = 0f;
+        obj_box.transform.position = temp;
+    }
+
+
+}
